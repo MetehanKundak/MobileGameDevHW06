b777df83417b48a18d7be4fc41b915733dd17212
Metehan <metehan@Metehan-MacBook-Pro.local>
2021-11-07 21:25:06 -0500

    Added buttons and layouts to main act.

--- app/src/main/java/com/example/hw06/MainActivity.java
+++ app/src/main/java/com/example/hw06/MainActivity.java
@@ -3,9 +3,16 @@ package com.example.hw06;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+
+    Button moveplayer;
+    LinearLayout layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

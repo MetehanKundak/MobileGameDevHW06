708faa3f32304cafcab7eacca31b632d81ca4de9
Metehan <metehan@Metehan-MacBook-Pro.local>
2021-11-14 20:24:47 -0500

    gamepanel class created

---
+++ app/src/main/java/com/example/hw06/GamePanel.java
@@ -0,0 +1,12 @@
+package com.example.hw06;
+
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.MainThread;
+
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
+
+private MainThread thread
+
+}
--- app/src/main/java/com/example/hw06/MainActivity.java
+++ app/src/main/java/com/example/hw06/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.hw06;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
@@ -16,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new GamePanel(this));
     }
 }

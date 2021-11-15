3d819f302fcc1ae07a10bcf161dff3fc6c4d2f8c
Metehan <metehan@Metehan-MacBook-Pro.local>
2021-11-14 20:31:28 -0500

    surface methods added

--- app/src/main/java/com/example/hw06/GamePanel.java
+++ app/src/main/java/com/example/hw06/GamePanel.java
@@ -1,5 +1,6 @@
 package com.example.hw06;
 
+import android.content.Context;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -7,6 +8,32 @@ import androidx.annotation.MainThread;
 
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
 
-private MainThread thread
+    private MainThread thread;
+
+    public GamePanel (Context context){
+
+        super(context);
+        getHolder().addCallback(this);
+
+        thread = new MainThread(getHolder(),this);
+
+        setFocusable(true);
+
+    }
+
+    @Override
+    public void surfaceChanged (SurfaceHolder holder, int format, int width, int height){
+
+
+    }
+
+    @Override
+    public void surfaceCreated (SurfaceHolder holder){
+
+        thread = new MainThread(getHolder(), this);
+        thread.setRunning(true);
+        thread.start();
+
+    }
 
 }
